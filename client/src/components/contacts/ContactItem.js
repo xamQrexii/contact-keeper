f2370420f5e7ca803fb0df4ab3627fe0da64b058
xamqrexii <usama1630@gmail.com>
2019-10-29 03:45:13 +0500

    Contacts and ContactItem components

---
+++ client/src/components/contacts/ContactItem.js
@@ -0,0 +1,40 @@
+import React, { Fragment } from 'react'
+import PropTypes from 'prop-types'
+
+const ContactItem = ({ contact }) => {
+
+    const { name, email, phone, type } = contact;
+
+    return (
+        <div className='card bg-light'>
+            <h3 className='text-parimary text-left'>
+                {name}{' '}
+                <span style={{ float: 'right' }} className={`badge ${type === 'professional' ? 'badge-success' : 'badge-primary'}`}>
+                    {type.charAt(0).toUpperCase() + type.slice(1)}
+                </span>
+            </h3>
+            <ul className='list'>
+                {email && (
+                    <li>
+                        <i className='fas fa-envelope-open' /> {email}
+                    </li>
+                )}
+                {phone && (
+                    <li>
+                        <i className='fas fa-phone' /> {phone}
+                    </li>
+                )}
+            </ul>
+            <p>
+                <button className='btn btn-primary btn-sm'>Edit</button>
+                <button className='btn btn-danger btn-sm'>Delete</button>
+            </p>
+        </div>
+    )
+}
+
+ContactItem.propTypes = {
+    contact: PropTypes.object.isRequired,
+}
+
+export default ContactItem;
---
+++ client/src/components/contacts/Contacts.js
@@ -0,0 +1,16 @@
+import React, { Fragment, useContext } from 'react'
+import ContactContext from '../../context/contact/ContactContext';
+import ContactItem from './ContactItem';
+
+const Contacts = () => {
+    const { contacts } = useContext(ContactContext);
+    return (
+        <Fragment>
+            {
+                contacts.map((contact) => <ContactItem key={contact.id} contact={contact} />)
+            }
+        </Fragment>
+    )
+}
+
+export default Contacts;
